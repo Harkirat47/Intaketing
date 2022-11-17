d2eab88b0fa2e24c085ca0f702ec1de869e06318
Harkirat47 <har.ki.kirat@gmail.com>
2022-11-16 17:27:40 -0800

    I am doin it

---
+++ Team5/src/main/java/frc/robot/commands/Inwards.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.Intake;
+import frc.robot.Constants;
+/** An example command that uses an example subsystem. */
+public class Inwards extends CommandBase {
+  private final Intake m_take;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public Inwards(Intake intake) {
+    m_take = intake;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+      m_take.goforward();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+      m_take.gobackwards();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- Team5/src/main/java/frc/robot/subsystems/intake.java
+++ Team5/src/main/java/frc/robot/subsystems/intake.java
@@ -1,5 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
-public class Intake {
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+  DoubleSolenoid Solinoid1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+  DoubleSolenoid Solinoid2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+  
+
+  public Intake() {
+        
+  pcmCompressor.enableDigital();
+  pcmCompressor.enabled();
+  Solinoid1.set(Value.kReverse);
+  Solinoid2.set(Value.kReverse);  
+  
+  }
+
+  public void goforward(){
+
+    Solinoid1.set(Value.kForward);
+    Solinoid2.set(Value.kForward);
     
+  }
+  public void gobackwards(){    
+
+    Solinoid1.set(Value.kReverse);
+    Solinoid2.set(Value.kReverse);  
+    
+    }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
 }
